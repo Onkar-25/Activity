fbf14afae469782c0835b1e2cbb419a307b92438
Onkar-25 <55799837+Onkar-25@users.noreply.github.com>
2020-06-18 20:04:37 +0530

    Update Electrcity_bill_calculation.c

--- Electrcity_bill_calculation.c
+++ Electrcity_bill_calculation.c
@@ -5,8 +5,14 @@ main()
    float total_bill;
    printf("Enter the number of units consumed for one month = ");
     scanf("%d",&units);
-   
+    /*Bill Slabs
+    Monthly consumption         Cost/unit(Rs)
 
+    up to 30 units                  3.25
+    between 31 and 100              4.40
+    between 101 and 200             6.25
+    above 200                       7.30
+    */
     if(units<=30)
     {
         total_bill=3.25*units;
