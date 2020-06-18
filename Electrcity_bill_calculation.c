fed4562dbbcb0af67e8708d1e0e8d36342d2acea
Onkar-25 <55799837+Onkar-25@users.noreply.github.com>
2020-06-18 19:58:57 +0530

    Create Electrcity_bill_calculation.c

---
+++ Electrcity_bill_calculation.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main()
+{
+    float total_bill;
+
+    printf("Enter the number of units consumed for one month = ");
+    scanf("%d",&units);
+   
+
+    if(units<=30)
+    {
+        total_bill=3.25*units;
+    }
+    else if(units>30 && units <=100)
+    {
+        total_bill=3.25*30+4.40*(units-30);
+    }
+    else if(units>100 && units<=200)
+    {
+        total_bill=3.25*30+4.40*70+6.25*(units-100);
+    }
+    
+
+    printf("\nTotal monthly bill = %.2f",total_bill);
+}
